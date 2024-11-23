60b172537debb6a07bc01940a68c2c60d449b9fd
Chaesunbak <coanstjd0422@gmail.com>
2024-11-23 22:20:58 +0900

    fix : select는 비동기 함수를 지원하지 않으므로 fetch로 좌표추가 로직 이동

--- src/hooks/useSiseWithReactQuery.ts
+++ src/hooks/useSiseWithReactQuery.ts
@@ -2,6 +2,7 @@ import { useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { fetchSiseDataThatThrowsError } from '../api/Sise.api';
 import { groupSiseByAddress } from '../utils/sortUtils';
+import { addXyToSiseOfBuilding } from '../utils/adress';
 
 // 이 훅을 사용하면 앱에서 데이터를 쉽게 공유할 수 있습니다.
 const useSiseWithReactQuery = () => {
@@ -14,20 +15,21 @@ const useSiseWithReactQuery = () => {
     const { data, isPending, isError, error } = useQuery({
         queryKey: ['siseData', regionCode],
         queryFn: async () => {
-            return await fetchSiseDataThatThrowsError({
+            const data = await fetchSiseDataThatThrowsError({
                 LAWD_CD: regionCode,
                 DEAL_YMD: Number(currentYYYYMM),
                 pageNo: 1,
                 numOfRows: 1000,
             });
-        },
-        select: (data) => {
+
             let item = data.response.body.items.item;
 
             if (!Array.isArray(item)) {
                 item = [item];
             }
             const groupedByAdressItems = groupSiseByAddress(item);
+
+            return await addXyToSiseOfBuilding(groupedByAdressItems);
         },
     });
 
