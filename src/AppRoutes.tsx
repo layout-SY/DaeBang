5bbaaeb6db98844869880a6ef7a61f9d05c56971
Chaesunbak <coanstjd0422@gmail.com>
2024-11-25 17:29:48 +0900

    feat : /onetwo로 리다이렉트 하는 홈페이지 추가

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import SiseList2 from './components/SiseList2';
+import Home from './components/Home';
 
 // 라우팅은 여기서 설정합니다.
 const AppRoutes = () => (
     <Routes>
         <Route path="/" element={<Layout />}>
+            {/* 현재 홈페이지는 내용이 없고 바로 /onetwo로 리다이렉트 됩니다. */}
+            <Route path="/" element={<Home />} />
             <Route path=":category" element={<SiseList2 />} />
             <Route
                 path="/bookmark"
