dfe6c4706eb2ca7d255d3aa1686563a9eb363ad2
Chaesunbak <coanstjd0422@gmail.com>
2024-11-26 18:14:55 +0900

    feat : 디테일리스트에 지도에서 보기 기능 구현

--- src/components/Detail/DetailViewOnMapButton.tsx
+++ src/components/Detail/DetailViewOnMapButton.tsx
@@ -1,23 +1,37 @@
 import styled from 'styled-components';
 import { FaMapMarkedAlt } from 'react-icons/fa';
 import { Position } from '../Map';
+import { useSearchParams } from 'react-router-dom';
 
 interface DetailViewOnMapButtonProps {
     position: Position;
 }
 
 const DetailViewOnMapButton = ({ position }: DetailViewOnMapButtonProps) => {
-    const handleCick = () => {
-        console.log('지도에 위치를 표시합니다.', position);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    // 클릭시 지도에서 해당 위치를 보여줍니다.
+    // 부드럽게 움직이기, 줌레벨 조정하기 등은 현재 구조에서는 불가능 하고,
+    // 이는 리덕스를 도입하거나 다른 방법을 사용해야 합니다.
+    const handleClick = () => {
+        const newSearchParams = new URLSearchParams(searchParams);
+        newSearchParams.set('lat', position.lat.toString());
+        newSearchParams.set('lng', position.lng.toString());
+        setSearchParams(newSearchParams);
     };
+
     return (
-        <StyledDetailViewOnMapButton onClick={handleCick}>
+        <StyledDetailViewOnMapButton
+            onClick={handleClick}
+            aria-label="지도에서 보기"
+        >
             <FaMapMarkedAlt />
         </StyledDetailViewOnMapButton>
     );
 };
 
 const StyledDetailViewOnMapButton = styled.button`
+    position: relative;
     border: none;
     background: transparent;
     outline: none;
@@ -30,7 +44,7 @@ const StyledDetailViewOnMapButton = styled.button`
 
     &:hover {
         svg {
-            fill: ${({ theme }) => theme.colors.primary};
+            fill: ${({ theme }) => theme.colors.blue};
         }
     }
 `;
