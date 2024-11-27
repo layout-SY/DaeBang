42e2ce8c6966bb07d9696d08bb2b62fbddca4ba6
Chaesunbak <coanstjd0422@gmail.com>
2024-11-28 01:10:00 +0900

    faet : 앱라우트에서 레이지 로딩 구현

---
+++ src/components/LoadingLayout.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import CategoryBar from './CategoryBar';
+import Loader from './common/Loader';
+import Sidebar from './Sidebar';
+
+const LoadingLayout = () => {
+    return (
+        <StyledLoadingLayout>
+            <CategoryBar />
+            <main className="sidebar-layout">
+                <Sidebar>
+                    <Loader />
+                </Sidebar>
+                <Loader />
+            </main>
+        </StyledLoadingLayout>
+    );
+};
+
+const StyledLoadingLayout = styled.div`
+    display: flex;
+    height: 100%;
+
+    .sidebar-layout {
+        flex: 1 1;
+    }
+`;
+
+export default LoadingLayout;
---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,40 @@
+import styled, { keyframes } from 'styled-components';
+
+const Loader = () => {
+    return (
+        <StyledLoader>
+            <Dot delay="0s" />
+            <Dot delay="0.1s" />
+            <Dot delay="0.2s" />
+        </StyledLoader>
+    );
+};
+
+const bounce = keyframes`
+  0%, 80%, 100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-15px);
+  }
+`;
+
+const StyledLoader = styled.div`
+    height: 100%;
+    margin: auto auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+`;
+
+const Dot = styled.div<{ delay: string }>`
+    width: 10px;
+    height: 10px;
+    background-color: ${({ theme }) => theme.colors.blue};
+    border-radius: 50%;
+    animation: ${bounce} 1.4s infinite ease-in-out;
+    animation-delay: ${({ delay }) => delay};
+`;
+
+export default Loader;
