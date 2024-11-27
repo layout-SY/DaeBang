aec84123226bcd6cd282bbe8d7a818338a5482b6
Chaesunbak <coanstjd0422@gmail.com>
2024-11-27 17:54:39 +0900

    feat : 카테고리별 시세 타입 정의

--- src/models/Sise.model.ts
+++ src/models/Sise.model.ts
@@ -36,6 +36,59 @@ export interface Sise {
     useRRRight: string;
 }
 
+interface BaseSise {
+    buildYear: number;
+    contractTerm: string;
+    contractType: string;
+    dealDay: number;
+    dealMonth: number;
+    dealYear: number;
+    deposit: string;
+    excluUseAr: number;
+    floor: number;
+    jibun: number;
+    monthlyRent: number;
+    preDeposit: string;
+    preMonthyRent: number;
+    sggCd: number;
+    umdNm: string;
+    useRRRight: string;
+}
+
+export type SiseCaegory = 'onetwo' | 'officetel' | 'apt';
+
+export interface OneTwoSise extends BaseSise {
+    houseType: string;
+    mhouseNm: string;
+}
+
+export interface AptSise extends BaseSise {
+    aptName: string;
+}
+
+export interface OfficetelSise extends BaseSise {
+    offiname: string;
+}
+
+export type AnySise = OneTwoSise | AptSise | OfficetelSise;
+
+export interface BaseSiseAPIResponse<T> {
+    response: {
+        body: {
+            items: {
+                item: T[] | T; // 배열이거나 단일 객체일 수 있음
+            };
+            numOfRows: number;
+            pageNo: number;
+            totalCount: number;
+        };
+        header: {
+            resultCode: string;
+            resultMsg: string;
+        };
+    };
+}
+
 export interface paginateByKeyResultProps {
     key: string;
     items: Sise[];
